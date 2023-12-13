81d3602951d6434de585530e18843400c3e8ac17
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-12-13 17:38:14 +0100

    ADD: filter and

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,9 @@ class PageController extends Controller
 {
     public function index() 
     {   
-        $trains = Train::where('departure_time','>','2023-12-13' )->get();
+        $trains = Train::where('departure_time','>','2023-12-13 00:00:00')
+                        ->where('departure_time','<','2023-12-13 23:59:59' )
+                        ->get();
         // dd($trains);
 
         return view('guest.home', compact('trains'));
